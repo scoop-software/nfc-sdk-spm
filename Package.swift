2e1fbcad2de9f8fb8ca36662e6b0fedb4fa22f61
Philipp Kursawe <philipp.kursawe@scoop-software.de>
2026-06-08 13:22:52 +0200

    feat: bump SPM binary targets to 1.18.0

--- Package.swift
+++ Package.swift
@@ -5,13 +5,19 @@ let package = Package(
     name: "ScoopNfc",
     platforms: [.iOS(.v14)],
     products: [
-        .library(name: "ScoopNfc", targets: ["ScoopNfc"])
+        .library(name: "ScoopNfc", targets: ["ScoopNfc"]),
+        .library(name: "ScoopNfcUI", targets: ["ScoopNfcUI"]),
     ],
     targets: [
         .binaryTarget(
             name: "ScoopNfc",
-            url: "https://github.com/scoop-software/nfc-sdk-spm/releases/download/v1.17.0/ScoopNfc.xcframework.zip",
-            checksum: "140ab78e8a04e32ccb5f09541ddfafec0730657d97ec00bc7a0545de4e7a9a39"
-        )
+            url: "https://github.com/scoop-software/nfc-sdk-spm/releases/download/v1.18.0/ScoopNfc.xcframework.zip",
+            checksum: "8b25b6ebda24519f9f31a3cfc4195f431676f74323d6dee59c7a7f72abd9ccaa"
+        ),
+        .binaryTarget(
+            name: "ScoopNfcUI",
+            url: "https://github.com/scoop-software/nfc-sdk-spm/releases/download/v1.18.0/ScoopNfcUI.xcframework.zip",
+            checksum: "6843a8da3b59d30c26871399201bd389c3efeea74861aea0051b2e0a59d4962a"
+        ),
     ]
 )
